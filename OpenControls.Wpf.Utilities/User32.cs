c94729aabb181459fafd4eb8140e9a10acc9e458
Ivan <vanny99.inf@gmail.com>
2021-09-13 18:22:43 +0300

    OpenControls merge.

---
+++ OpenControls.Wpf.Utilities/User32.cs
@@ -0,0 +1,95 @@
+﻿using System.Runtime.InteropServices;
+using System;
+using System.Windows;
+
+namespace OpenControls.Wpf.Utilities
+{
+    public class User32
+    {
+        public delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
+
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+        }
+
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorPos(out POINT lpPoint);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        public static extern short GetAsyncKeyState(int vkey);
+
+        [DllImport("User32.dll")]
+        static extern IntPtr GetDC(IntPtr hwnd);
+
+        [DllImport("gdi32.dll")]
+        static extern int GetDeviceCaps(IntPtr hdc, int nIndex);
+
+        [DllImport("user32.dll")]
+        static extern bool ReleaseDC(IntPtr hWnd, IntPtr hDC);
+
+#pragma warning disable 649
+
+        internal struct INPUT
+        {
+            public UInt32 Type;
+            public MOUSEKEYBDHARDWAREINPUT Data;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        internal struct MOUSEKEYBDHARDWAREINPUT
+        {
+            [FieldOffset(0)]
+            public MOUSEINPUT Mouse;
+        }
+
+        internal struct MOUSEINPUT
+        {
+            public Int32 X;
+            public Int32 Y;
+            public UInt32 MouseData;
+            public UInt32 Flags;
+            public UInt32 Time;
+            public IntPtr ExtraInfo;
+        }
+
+        internal static uint LEFTMOUSEDOWN = 0x0002;
+        internal static uint LEFTMOUSEUP = 0x0004;
+        internal static uint MOUSEINPUTTYPE = 0x0000;
+
+#pragma warning restore 649
+
+        [DllImport("user32.dll")]
+        internal static extern uint SendInput(uint nInputs, [MarshalAs(UnmanagedType.LPArray), In] INPUT[] pInputs, int cbSize);
+
+        public static Point ConvertPixelsToUnits(Point point)
+        {
+            double physicalUnitSize = GetPhysicalUnitSize();
+            return new Point(point.X / physicalUnitSize, point.Y / physicalUnitSize);
+        }
+
+        public static Point ConvertPixelsToUnits(int x, int y)
+        {
+            var physicalUnitSize = GetPhysicalUnitSize();
+            return new Point((double)x / physicalUnitSize, (double)y / physicalUnitSize);
+        }
+
+        private static double GetPhysicalUnitSize()
+        {
+            // get the system DPI
+            IntPtr dDC = GetDC(IntPtr.Zero); // Get desktop DC
+            int dpi = GetDeviceCaps(dDC, 88);
+            bool rv = ReleaseDC(IntPtr.Zero, dDC);
+
+            // WPF's physical unit size is calculated by taking the 
+            // "Device-Independant Unit Size" (always 1/96)
+            // and scaling it by the system DPI
+            return (1d / 96d) * (double)dpi;
+        }
+    }
+}
---
+++ OpenControls.Wpf.Utilities/ValueConverters/ObjectToBooleanConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace OpenControls.Wpf.Utilities.ValueConverters
+{
+    [ValueConversion(typeof(object), typeof(Boolean))]
+    public class ObjectToBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (targetType != typeof(Boolean))
+            {
+                throw new NotSupportedException();
+            }
+
+            return (value != null);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
